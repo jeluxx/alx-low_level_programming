a58b697f32e3572a0baecae9e1ddf35073b6adfd
jeluxx <jluxgerald@gmail.com>
2022-12-12 19:26:22 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - A c program that prints a line to the standard error
  * Return 1 (Success)
  */
-int main(void)
+in main(void)
 {
-	fput9("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return(1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
